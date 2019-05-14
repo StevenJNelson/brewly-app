19e3f57837fdab53b3d9bd011c8dbbdfb49616d5
Steven <Steven.Nelson96@gmail.com>
2019-05-14 13:01:52 -0500

    create brewly app

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1> Welcome to Brewly </h1>
+        </header>
+      </div>
+    );
+  }
+}
+export default Home;
